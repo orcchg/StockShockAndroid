13e7233c74fe7eaff25fae2e968189a73ee2055c
Maxim Alov <Alov.M.D@sberbank.ru>
2021-04-03 13:57:58 +0300

    Get rid of stupid code in Dependencies

--- buildSrc/src/main/kotlin/com/orcchg/yandexcontest/build/Dependencies.kt
+++ buildSrc/src/main/kotlin/com/orcchg/yandexcontest/build/Dependencies.kt
@@ -49,53 +49,53 @@ object Versions {
 object Dependencies {
 
     object AndroidX {
-        @JvmStatic val appCompat = dependency("androidx.appcompat:appcompat:${Versions.appCompat}")
-        @JvmStatic val coreKtx = dependency("androidx.core:core-ktx:${Versions.coreKtx}")
-        @JvmStatic val fragmentKtx = dependency("androidx.fragment:fragment-ktx:${Versions.fragmentKtx}")
-        @JvmStatic val liveData = dependency("androidx.lifecycle:lifecycle-livedata-ktx:${Versions.lifecycle}")
-        @JvmStatic val material = dependency("com.google.android.material:material:${Versions.material}")
-        @JvmStatic val navigation = dependency("androidx.navigation:navigation-fragment-ktx:${Versions.navigation}")
-        @JvmStatic val navigationSafeArgs = dependency("androidx.navigation:navigation-safe-args-gradle-plugin:${Versions.navigation}")
-        @JvmStatic val navigationUi = dependency("androidx.navigation:navigation-ui-ktx:${Versions.navigation}")
-        @JvmStatic val room = dependency("androidx.room:room-runtime:${Versions.room}")
-        @JvmStatic val roomCompiler = dependency("androidx.room:room-compiler:${Versions.room}")
-        @JvmStatic val roomRx = dependency("androidx.room:room-rxjava2:${Versions.room}")
+        @JvmStatic val appCompat = "androidx.appcompat:appcompat:${Versions.appCompat}"
+        @JvmStatic val coreKtx = "androidx.core:core-ktx:${Versions.coreKtx}"
+        @JvmStatic val fragmentKtx = "androidx.fragment:fragment-ktx:${Versions.fragmentKtx}"
+        @JvmStatic val liveData = "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.lifecycle}"
+        @JvmStatic val material = "com.google.android.material:material:${Versions.material}"
+        @JvmStatic val navigation = "androidx.navigation:navigation-fragment-ktx:${Versions.navigation}"
+        @JvmStatic val navigationSafeArgs = "androidx.navigation:navigation-safe-args-gradle-plugin:${Versions.navigation}"
+        @JvmStatic val navigationUi = "androidx.navigation:navigation-ui-ktx:${Versions.navigation}"
+        @JvmStatic val room = "androidx.room:room-runtime:${Versions.room}"
+        @JvmStatic val roomCompiler = "androidx.room:room-compiler:${Versions.room}"
+        @JvmStatic val roomRx = "androidx.room:room-rxjava2:${Versions.room}"
     }
 
     object Di {
-        @JvmStatic val dagger = dependency("com.google.dagger:dagger:${Versions.dagger}")
-        @JvmStatic val daggerCompiler = dependency("com.google.dagger:dagger-compiler:${Versions.dagger}")
+        @JvmStatic val dagger = "com.google.dagger:dagger:${Versions.dagger}"
+        @JvmStatic val daggerCompiler = "com.google.dagger:dagger-compiler:${Versions.dagger}"
     }
 
     object Gradle {
-        @JvmStatic val androidGradle = dependency("com.android.tools.build:gradle:${Versions.gradleAndroid}")
-        @JvmStatic val ktGradle = dependency("org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.ktStdLib}")
+        @JvmStatic val androidGradle = "com.android.tools.build:gradle:${Versions.gradleAndroid}"
+        @JvmStatic val ktGradle = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.ktStdLib}"
     }
 
     object Lib {
-        @JvmStatic val glide = dependency("com.github.bumptech.glide:glide:${Versions.glide}")
-        @JvmStatic val ktStdLib = dependency("org.jetbrains.kotlin:kotlin-stdlib:${Versions.ktStdLib}")
-        @JvmStatic val timber = dependency("com.jakewharton.timber:timber:${Versions.timber}")
+        @JvmStatic val glide = "com.github.bumptech.glide:glide:${Versions.glide}"
+        @JvmStatic val ktStdLib = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.ktStdLib}"
+        @JvmStatic val timber = "com.jakewharton.timber:timber:${Versions.timber}"
     }
 
     object Network {
-        @JvmStatic val finnhub = dependency("com.finnhub:kotlin-client:${Versions.finnhub}")
-        @JvmStatic val okHttpLog = dependency("com.squareup.okhttp3:logging-interceptor:${Versions.okHttp}")
-        @JvmStatic val retrofit = dependency("com.squareup.retrofit2:retrofit:${Versions.retrofit}")
-        @JvmStatic val retrofitMoshi = dependency("com.squareup.retrofit2:converter-moshi:${Versions.retrofit}")
-        @JvmStatic val retrofitRx = dependency("com.squareup.retrofit2:adapter-rxjava2:${Versions.retrofit}")
-        @JvmStatic val scarlet = dependency("com.tinder.scarlet:scarlet:${Versions.scarlet}")
-        @JvmStatic val scarletAndroid = dependency("com.tinder.scarlet:lifecycle-android:${Versions.scarlet}")
-        @JvmStatic val scarletMoshi = dependency("com.tinder.scarlet:message-adapter-moshi:${Versions.scarlet}")
-        @JvmStatic val scarletOkHttp = dependency("com.tinder.scarlet:websocket-okhttp:${Versions.scarlet}")
-        @JvmStatic val scarletRx = dependency("com.tinder.scarlet:stream-adapter-rxjava2:${Versions.scarlet}")
-        @JvmStatic val stetho = dependency("com.facebook.stetho:stetho-okhttp3:${Versions.stetho}")
+        @JvmStatic val finnhub = "com.finnhub:kotlin-client:${Versions.finnhub}"
+        @JvmStatic val okHttpLog = "com.squareup.okhttp3:logging-interceptor:${Versions.okHttp}"
+        @JvmStatic val retrofit = "com.squareup.retrofit2:retrofit:${Versions.retrofit}"
+        @JvmStatic val retrofitMoshi = "com.squareup.retrofit2:converter-moshi:${Versions.retrofit}"
+        @JvmStatic val retrofitRx = "com.squareup.retrofit2:adapter-rxjava2:${Versions.retrofit}"
+        @JvmStatic val scarlet = "com.tinder.scarlet:scarlet:${Versions.scarlet}"
+        @JvmStatic val scarletAndroid = "com.tinder.scarlet:lifecycle-android:${Versions.scarlet}"
+        @JvmStatic val scarletMoshi = "com.tinder.scarlet:message-adapter-moshi:${Versions.scarlet}"
+        @JvmStatic val scarletOkHttp = "com.tinder.scarlet:websocket-okhttp:${Versions.scarlet}"
+        @JvmStatic val scarletRx = "com.tinder.scarlet:stream-adapter-rxjava2:${Versions.scarlet}"
+        @JvmStatic val stetho = "com.facebook.stetho:stetho-okhttp3:${Versions.stetho}"
     }
 
     object Parser {
-        @JvmStatic val moshi = dependency("com.squareup.moshi:moshi-adapters:${Versions.moshi}")
-        @JvmStatic val moshiKotlin = dependency("com.squareup.moshi:moshi-kotlin:${Versions.moshi}")
-        @JvmStatic val moshiKotlinCodegen = dependency("com.squareup.moshi:moshi-kotlin-codegen:${Versions.moshi}")
+        @JvmStatic val moshi = "com.squareup.moshi:moshi-adapters:${Versions.moshi}"
+        @JvmStatic val moshiKotlin = "com.squareup.moshi:moshi-kotlin:${Versions.moshi}"
+        @JvmStatic val moshiKotlinCodegen = "com.squareup.moshi:moshi-kotlin-codegen:${Versions.moshi}"
     }
 
     object Plugin {
@@ -115,49 +115,41 @@ object Dependencies {
     }
 
     object Rx {
-        @JvmStatic val autoDispose = dependency("com.uber.autodispose:autodispose:${Versions.autoDispose}")
-        @JvmStatic val autoDisposeAndroidAC = dependency("com.uber.autodispose:autodispose-android-archcomponents:${Versions.autoDispose}")
-        @JvmStatic val rx = dependency("io.reactivex.rxjava2:rxjava:${Versions.rx}")
-        @JvmStatic val rxKt = dependency("io.reactivex.rxjava2:rxkotlin:${Versions.rxKt}")
-        @JvmStatic val rxAndroid = dependency("io.reactivex.rxjava2:rxandroid:${Versions.rxAndroid}")
-        @JvmStatic val rxBinding = dependency("com.jakewharton.rxbinding3:rxbinding:${Versions.rxBinding}")
-        @JvmStatic val rxBindingCore = dependency("com.jakewharton.rxbinding3:rxbinding-core:${Versions.rxBinding}")
-        @JvmStatic val rxBindingSwipe = dependency("com.jakewharton.rxbinding3:rxbinding-swiperefreshlayout:${Versions.rxBinding}")
+        @JvmStatic val autoDispose = "com.uber.autodispose:autodispose:${Versions.autoDispose}"
+        @JvmStatic val autoDisposeAndroidAC = "com.uber.autodispose:autodispose-android-archcomponents:${Versions.autoDispose}"
+        @JvmStatic val rx = "io.reactivex.rxjava2:rxjava:${Versions.rx}"
+        @JvmStatic val rxKt = "io.reactivex.rxjava2:rxkotlin:${Versions.rxKt}"
+        @JvmStatic val rxAndroid = "io.reactivex.rxjava2:rxandroid:${Versions.rxAndroid}"
+        @JvmStatic val rxBinding = "com.jakewharton.rxbinding3:rxbinding:${Versions.rxBinding}"
+        @JvmStatic val rxBindingCore = "com.jakewharton.rxbinding3:rxbinding-core:${Versions.rxBinding}"
+        @JvmStatic val rxBindingSwipe = "com.jakewharton.rxbinding3:rxbinding-swiperefreshlayout:${Versions.rxBinding}"
     }
 
     object View {
-        @JvmStatic val constraintLayout = dependency("androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}")
-        @JvmStatic val recyclerView = dependency("androidx.recyclerview:recyclerview:${Versions.recyclerView}")
-        @JvmStatic val shimmer = dependency("com.facebook.shimmer:shimmer:${Versions.shimmer}")
-        @JvmStatic val swipeRefreshLayout = dependency("androidx.swiperefreshlayout:swiperefreshlayout:${Versions.swipeRefreshLayout}")
-        @JvmStatic val viewPager2 = dependency("androidx.viewpager2:viewpager2:${Versions.viewPager2}")
+        @JvmStatic val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayout}"
+        @JvmStatic val recyclerView = "androidx.recyclerview:recyclerview:${Versions.recyclerView}"
+        @JvmStatic val shimmer = "com.facebook.shimmer:shimmer:${Versions.shimmer}"
+        @JvmStatic val swipeRefreshLayout = "androidx.swiperefreshlayout:swiperefreshlayout:${Versions.swipeRefreshLayout}"
+        @JvmStatic val viewPager2 = "androidx.viewpager2:viewpager2:${Versions.viewPager2}"
     }
 
     object Lint {
-        @JvmStatic val detektApi = dependency("io.gitlab.arturbosch.detekt:detekt-api:${Versions.detekt}")
-        @JvmStatic val detektFormatting = dependency("io.gitlab.arturbosch.detekt:detekt-formatting:${Versions.detekt}")
-        @JvmStatic val detektTest = dependency("io.gitlab.arturbosch.detekt:detekt-test:${Versions.detekt}")
-        @JvmStatic val lintApi = dependency("com.android.tools.lint:lint-api:${Versions.lint}")
-        @JvmStatic val lintChecks = dependency("com.android.tools.lint:lint-checks:${Versions.lint}")
-        @JvmStatic val lintCore = dependency("com.android.tools.lint:lint:${Versions.lint}")
-        @JvmStatic val lintTests = dependency("com.android.tools.lint:lint-tests:${Versions.lint}")
+        @JvmStatic val detektApi = "io.gitlab.arturbosch.detekt:detekt-api:${Versions.detekt}"
+        @JvmStatic val detektFormatting = "io.gitlab.arturbosch.detekt:detekt-formatting:${Versions.detekt}"
+        @JvmStatic val detektTest = "io.gitlab.arturbosch.detekt:detekt-test:${Versions.detekt}"
+        @JvmStatic val lintApi = "com.android.tools.lint:lint-api:${Versions.lint}"
+        @JvmStatic val lintChecks = "com.android.tools.lint:lint-checks:${Versions.lint}"
+        @JvmStatic val lintCore = "com.android.tools.lint:lint:${Versions.lint}"
+        @JvmStatic val lintTests = "com.android.tools.lint:lint-tests:${Versions.lint}"
     }
 
     object AndroidTest {
-        @JvmStatic val espresso = dependency("androidx.test.espresso:espresso-core:${Versions.espresso}")
-        @JvmStatic val junit = dependency("androidx.test.ext:junit:${Versions.junitAndroidExt}")
+        @JvmStatic val espresso = "androidx.test.espresso:espresso-core:${Versions.espresso}"
+        @JvmStatic val junit = "androidx.test.ext:junit:${Versions.junitAndroidExt}"
     }
 
     object Test {
-        @JvmStatic val hamcrest = dependency("org.hamcrest:hamcrest-all:${Versions.hamcrest}")
-        @JvmStatic val junit = dependency("junit:junit:${Versions.junit}")
-    }
-
-    @Suppress("MemberVisibilityCanBePrivate")
-    val allProjectDependencies: MutableSet<String> = mutableSetOf()
-
-    private fun dependency(dependency: String): String {
-        allProjectDependencies.add(dependency)
-        return dependency
+        @JvmStatic val hamcrest = "org.hamcrest:hamcrest-all:${Versions.hamcrest}"
+        @JvmStatic val junit = "junit:junit:${Versions.junit}"
     }
 }
