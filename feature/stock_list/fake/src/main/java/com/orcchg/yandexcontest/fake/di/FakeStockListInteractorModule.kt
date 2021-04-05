d6ea34dc75b0118153ade89eee6aa3f7fe881c7e
Maxim Alov <Alov.M.D@sberbank.ru>
2021-04-05 11:55:51 +0300

    Improve accessibility for FakeStockListInteractor impl

--- feature/stock_list/fake/src/main/java/com/orcchg/yandexcontest/fake/di/FakeStockListInteractorModule.kt
+++ feature/stock_list/fake/src/main/java/com/orcchg/yandexcontest/fake/di/FakeStockListInteractorModule.kt
@@ -5,11 +5,13 @@ import com.orcchg.yandexcontest.fake.FakeStockListInteractor
 import com.orcchg.yandexcontest.stocklist.api.StockListInteractor
 import dagger.Binds
 import dagger.Module
+import dagger.Reusable
 
 @Module
 @PublishedWithReasonableAlternatives(binding = StockListInteractor::class)
 interface FakeStockListInteractorModule {
 
     @Binds
+    @Reusable
     fun interactor(impl: FakeStockListInteractor): StockListInteractor
 }
