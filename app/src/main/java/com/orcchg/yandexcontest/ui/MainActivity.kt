42273d91bfd66a648d53b2c5002e002f2573390e
Maxim Alov <Alov.M.D@sberbank.ru>
2021-04-09 21:33:16 +0300

    Fix known library leak on Android Q reported by leakCanary

--- app/src/main/java/com/orcchg/yandexcontest/ui/MainActivity.kt
+++ app/src/main/java/com/orcchg/yandexcontest/ui/MainActivity.kt
@@ -1,6 +1,22 @@
 package com.orcchg.yandexcontest.ui
 
+import android.os.Build
+import androidx.navigation.findNavController
 import com.orcchg.yandexcontest.R
 import com.orcchg.yandexcontest.coreui.BaseActivity
 
-class MainActivity : BaseActivity(R.layout.activity_main)
+class MainActivity : BaseActivity(R.layout.activity_main) {
+
+    override fun onBackPressed() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val nav = findNavController(R.id.main_nav_subhost_fragment)
+            val destination = nav.currentDestination
+            if (destination == null || destination.id == R.id.main_stock_pages_fragment) {
+                finishAfterTransition()
+                return
+            }
+        }
+
+        super.onBackPressed()
+    }
+}
