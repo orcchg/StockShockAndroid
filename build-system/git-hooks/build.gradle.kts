d2b7d5ccb6ed23c7e94b9a598d478c567eab2c82
Maxim Alov <Alov.M.D@sberbank.ru>
2021-04-16 16:50:54 +0300

    Add :build-system as included build and stub for :git-hooks infra module

--- build-system/git-hooks/build.gradle.kts
+++ build-system/git-hooks/build.gradle.kts
@@ -1,14 +1,14 @@
 plugins {
+    `java-gradle-plugin`
     `kotlin-dsl`
 }
 
 repositories {
-    google()
-    mavenCentral()
-    jcenter()
+    gradlePluginPortal()
+    maven { url = uri("https://plugins.gradle.org/m2/") }
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:4.1.3")
+    gradleApi()
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
 }
