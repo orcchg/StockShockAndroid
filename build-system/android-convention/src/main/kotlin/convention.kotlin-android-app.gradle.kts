ee23600e4bbacbafbd499982136e039a3f99b78b
Maxim Alov <Alov.M.D@sberbank.ru>
2021-04-20 22:47:33 +0300

    Provide release variant for :app module because it is need for dynamic feature to assemble in release variant

--- build-system/android-convention/src/main/kotlin/convention.kotlin-android-app.gradle.kts
+++ build-system/android-convention/src/main/kotlin/convention.kotlin-android-app.gradle.kts
@@ -1,13 +1,8 @@
 plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    id("convention.kotlin-base")
-    id("convention.android-base")
+    id("convention.kotlin-android-base-app")
 }
 
-@Suppress("UnstableApiUsage")
 android {
-    testBuildType = "debug"
     /**
      * Disable all buildTypes except testing
      * to avoid confusing errors in IDE if wrong build variant is selected
@@ -17,20 +12,5 @@ android {
             ignore = true
             logger.debug("Build variant $name is omitted for module: $path")
         }
-    }
-
-    buildFeatures {
-        buildConfig = true
-    }
-
-    buildTypes {
-        getByName(testBuildType) {
-            // libraries only built in release variant, see convention.kotlin-android-library
-            matchingFallbacks += listOf("release")
-        }
-    }
-
-    packagingOptions {
-        exclude("META-INF/*.kotlin_module")
     }
 }
---
+++ build-system/android-convention/src/main/kotlin/convention.kotlin-android-base-app.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("convention.kotlin-base")
+    id("convention.android-base")
+}
+
+@Suppress("UnstableApiUsage")
+android {
+    testBuildType = "debug"
+
+    buildFeatures {
+        buildConfig = true
+    }
+
+    buildTypes {
+        getByName(testBuildType) {
+            // libraries only built in release variant, see convention.kotlin-android-library
+            matchingFallbacks += listOf("release")
+        }
+    }
+
+    packagingOptions {
+        exclude("META-INF/*.kotlin_module")
+    }
+}
