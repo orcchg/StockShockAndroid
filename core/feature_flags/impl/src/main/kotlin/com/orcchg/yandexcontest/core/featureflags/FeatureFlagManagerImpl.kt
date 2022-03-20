565a4f50eec3bd4178f5b93f6f4ec1115c559d6e
Maxim Alov <alovmax@yandex.ru>
2022-03-20 18:19:16 +0400

    Fixed build config generation for pure kotlin module

--- core/feature_flags/impl/src/main/kotlin/com/orcchg/yandexcontest/core/featureflags/FeatureFlagManagerImpl.kt
+++ core/feature_flags/impl/src/main/kotlin/com/orcchg/yandexcontest/core/featureflags/FeatureFlagManagerImpl.kt
@@ -1,13 +1,13 @@
 package com.orcchg.yandexcontest.core.featureflags
 
-//import YandexMobileContest2021.core.feature_flags.BuildConfig
+import StockShockAndroid.core.feature_flags.BuildConfig
 import com.orcchg.yandexcontest.core.featureflags.api.FeatureFlagManager
 import javax.inject.Inject
 
 class FeatureFlagManagerImpl @Inject constructor() : FeatureFlagManager {
 
     override fun isRealTimeQuotesEnabled(): Boolean =
-        true//BuildConfig.FEATURE_FLAG_REAL_TIME_QUOTES_ENABLED
+        BuildConfig.FEATURE_FLAG_REAL_TIME_QUOTES_ENABLED
 
-    override fun getStockIndex(): String = ""//BuildConfig.DEFAULT_STOCK_INDEX
+    override fun getStockIndex(): String = BuildConfig.DEFAULT_STOCK_INDEX
 }
