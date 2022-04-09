0d36bf06b9ffdbbfbd88ffe2caf05f94814e3480
orcchg <orcchg@gmail.com>
2022-04-09 22:46:56 +0400

    Fix stock_list:data:fake compilation

--- core/network/api/src/main/kotlin/com/orcchg/yandexcontest/core/network/api/NetworkApi.kt
+++ core/network/api/src/main/kotlin/com/orcchg/yandexcontest/core/network/api/NetworkApi.kt
@@ -3,11 +3,14 @@ package com.orcchg.yandexcontest.core.network.api
 import com.orcchg.yandexcontest.coredi.Api
 import com.squareup.moshi.Moshi
 import com.tinder.scarlet.Scarlet
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 
 interface NetworkApi : Api {
 
     fun moshi(): Moshi
+
+    fun okHttpClient(): OkHttpClient
 
     fun retrofit(): Retrofit.Builder
 
