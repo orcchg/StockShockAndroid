b3748ab4fab2763ed4d090170f3bb648922d6b32
orcchg <orcchg@gmail.com>
2022-04-13 17:24:08 +0400

    Fix 'Unable to find variant 'release' to use for SonarQube analysis. Candidates are: debug'

--- build-system/plugins/sonarqube/src/main/kotlin/convention.sonarqube-android.gradle.kts
+++ build-system/plugins/sonarqube/src/main/kotlin/convention.sonarqube-android.gradle.kts
@@ -11,15 +11,14 @@ plugins {
 description = "Plugin, which configures SonarQube for this Android project"
 
 configure<SonarQubeExtension> {
-    androidVariant = "release"
     properties {
         property(
             "sonar.coverage.jacoco.xmlReportPaths",
-            "$buildDir/reports/jacoco/$androidVariant/jacoco.xml"
+            "$buildDir/reports/jacoco/release/jacoco.xml,$buildDir/reports/jacoco/debug/jacoco.xml"
         )
         property(
             "sonar.java.binaries",
-            "$buildDir/intermediates/javac/$androidVariant/classes"
+            "$buildDir/intermediates/javac/release/classes,$buildDir/intermediates/javac/debug/classes"
         )
     }
 }
