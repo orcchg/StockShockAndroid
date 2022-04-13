298d07562d0e017849f6b3813f01b7f77a008e13
orcchg <orcchg@gmail.com>
2022-04-13 10:11:09 +0400

    Make root project's sonarqube task depend on all jacocoTestReport<Variant> tasks collected from all relevant subprojects

--- build-system/plugins/sonarqube/src/main/kotlin/convention.sonarqube-root.gradle.kts
+++ build-system/plugins/sonarqube/src/main/kotlin/convention.sonarqube-root.gradle.kts
@@ -1,5 +1,7 @@
 import org.gradle.kotlin.dsl.configure
 import org.sonarqube.gradle.SonarQubeExtension
+import org.sonarqube.gradle.SonarQubeTask
+import java.util.stream.Collectors
 
 /**
  * Convention plugin, which configures SonarQube the root project.
@@ -18,4 +20,20 @@ configure<SonarQubeExtension> {
         property("sonar.projectKey", "orcchg_StockShockAndroid")
         property("sonar.verbose", true)
     }
+}
+
+/**
+ * Set root project's [SonarQubeTask] depend on set of [JacocoReport] tasks
+ * collected from all relevant subprojects.
+ */
+tasks.withType<SonarQubeTask>().configureEach {
+    rootProject.subprojects.stream()
+        .filter { p ->
+            val sonarQubeExtension = p.extensions.findByType<SonarQubeExtension>()
+            p.plugins.hasPlugin(JacocoPlugin::class) &&
+            sonarQubeExtension != null && !sonarQubeExtension.isSkipProject
+        }
+        .map { p -> p.tasks.withType<JacocoReport>() }
+        .flatMap(Collection<JacocoReport>::stream)
+        .collect(Collectors.toList())
 }
