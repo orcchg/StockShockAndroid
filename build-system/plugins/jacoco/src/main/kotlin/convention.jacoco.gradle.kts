89dff0bf5d8007c81391f90732b6ac0e468f71db
orcchg <orcchg@gmail.com>
2022-04-20 01:13:19 +0400

    Adjust description for convention plugins related to jacoco

--- build-system/plugins/jacoco/src/main/kotlin/convention.jacoco.gradle.kts
+++ build-system/plugins/jacoco/src/main/kotlin/convention.jacoco.gradle.kts
@@ -2,7 +2,7 @@ import com.orcchg.stockshock.infra.plugins.utility.withVersionCatalogs
 import org.gradle.testing.jacoco.plugins.JacocoPluginExtension
 
 /**
- * Convention plugin, which configures JaCoCo for pure kotlin subprojects.
+ * Convention plugin, which configures JaCoCo for subprojects.
  *
  * @see https://docs.gradle.org/current/userguide/jacoco_plugin.html
  */
@@ -11,7 +11,7 @@ plugins {
     id("jacoco")
 }
 
-description = "Plugin, which configures JaCoCo for pure kotlin subprojects"
+description = "Plugin, which configures JaCoCo for subprojects"
 
 withVersionCatalogs {
     configure<JacocoPluginExtension> {
