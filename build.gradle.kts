dcc66b1428097c59833a43459ff7eb062001af63
orcchg <orcchg@gmail.com>
2022-06-05 11:20:34 +0400

    Remove no longer used reference to jacoco 3rd-party plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,4 @@
 buildscript {
-    repositories {
-        // for 'com.vanniktech:gradle-android-junit-jacoco-plugin:0.17.0-SNAPSHOT'
-        maven { setUrl(uri("https://oss.sonatype.org/content/repositories/snapshots")) }
-    }
     dependencies {
         classpath(libs.plugin.android.cachefix)
         classpath(libs.plugin.android.gradle)
@@ -12,7 +8,7 @@ buildscript {
 
 plugins {
     id("com.github.ben-manes.versions") version "0.38.0"
-//    id("com.autonomousapps.dependency-analysis") version "1.1.0"
+    id("com.autonomousapps.dependency-analysis") version "1.1.0"
     id("convention.jacoco-root")
     id("convention.sonarqube-root")
     id("git-hooks-install")
